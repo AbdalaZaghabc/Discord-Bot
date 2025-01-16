059e7f10b9f6f6fcd0d39cfc75716055afa5e42d
Abdala Zaghab <azaghabolivier@gmail.com>
2025-01-16 02:27:56 -0500

    Updates
    .

--- main.py
+++ main.py
@@ -12,16 +12,13 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 # remove the default help command so that we can write our own
 bot.remove_command('help')
 
-# Function to read the token from a file
-def get_token_from_file(filepath):
-    with open(filepath, 'r') as file:
-        return file.read().strip()
 
 async def main():
-    token = get_token_from_file('token.txt')  # Replace 'token.txt' with your token file path
+    #token = get_token_from_file('token.txt')  # Replace 'token.txt' with your token file path
     async with bot:
         await bot.add_cog(help_cog(bot))
         await bot.add_cog(music_cog(bot))
-        await bot.start(token)
+        #Token is placed here!
+        await bot.start("")
 
 asyncio.run(main())
